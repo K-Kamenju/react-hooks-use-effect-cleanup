756be09c31fe1a01f1aef8bb9b484b1a15e2b645
Caleb <kamenjuhkiiru@gmail.com>
2023-10-30 15:38:56 +0300

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,12 +4,19 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
+      console.log("running")
     }, 1000);
+
+    console.log("render")
+    // returning a cleanup function
+    return function cleanup() {
+      console.log("cleanup")
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
 }
-
 export default Clock;
